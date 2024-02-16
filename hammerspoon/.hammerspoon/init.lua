5abc4a9e56cf344e4a66ecd96c4c23350844dcfb
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2024-02-16 14:47:00 -0300

    Added hammerspoon config

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,20 @@
+local function openApp(name)
+	local script = [[
+        tell application "]] .. name .. [["
+            activate
+        end tell
+    ]]
+	hs.applescript(script)
+end
+
+hs.hotkey.bind({ "ctrl" }, "2", function()
+	openApp("Brave Browser")
+end)
+
+hs.hotkey.bind({ "ctrl" }, "3", function()
+	openApp("kitty")
+end)
+
+hs.hotkey.bind({ "ctrl" }, "4", function()
+	openApp("Slack")
+end)
