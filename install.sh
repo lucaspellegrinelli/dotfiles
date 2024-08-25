438baa98294df31339edc54b996246e6db1a42b8
Lucas Pellegrinelli <lucas.r.pellegrinelli@gmail.com>
2024-08-25 11:26:08 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+create_symlinks() {
+  script_dir=$(dirname "$(readlink -f "$0")")
+  files=$(find . -maxdepth 1 -type f -name ".*")
+  for file in $files; do
+    name=$(basename "$file")
+    echo "Creating symlink to $name in home directory."
+    rm -rf "${HOME:?}/$name"
+    ln -s "$script_dir/$name" "$HOME/$name"
+  done
+
+  if [ ! -d ~/.config/nvim ]
+  then
+    mkdir -p ~/.config/
+    ln -s "$script_dir/nvim" ~/.config/
+  fi
+}
+
+create_symlinks
